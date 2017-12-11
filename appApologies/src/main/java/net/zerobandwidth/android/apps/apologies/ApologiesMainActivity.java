07cc2b60f6a75ab442defb0c541c445b18ad5338
zerobandwidth <pasha.paterson@gmail.com>
2017-12-11 16:59:14 -0500

    (#1) Successfully linked other zerobandwidth.net libraries into project.

---
+++ appApologies/src/main/java/net/zerobandwidth/android/apps/apologies/ApologiesMainActivity.java
@@ -0,0 +1,15 @@
+package net.zerobandwidth.android.apps.apologies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ApologiesMainActivity
+extends AppCompatActivity
+{
+	@Override
+	protected void onCreate( Bundle bndlState )
+	{
+		super.onCreate(bndlState) ;
+		this.setContentView( R.layout.activity_apologies_main ) ;
+	}
+}
